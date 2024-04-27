e701287f9cd58f202ad05c4036bace9308765e65
Douglas Barreto <doouglas_barreto@hotmail.com>
2024-04-27 19:06:51 -0300

    Criação Titulo Tela Princial

--- obj/Debug/net8.0-windows/exemploTeste.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/exemploTeste.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemploTeste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ad7cfd98a6c45b380e53374919befdfb733edc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd965aaf51266336a10d2a8ecab370032efea982")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemploTeste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemploTeste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
